5f47b70b7c57f28b431229c9ad79e538c809c783
Mustafa Yiğit <mustafa.yt65@gmail.com>
2020-03-29 04:19:33 +0300

    Appbar Menu added

--- app/src/main/java/com/mustafayigit/kotlin_milliyetapp/ui/DashboardActivity.kt
+++ app/src/main/java/com/mustafayigit/kotlin_milliyetapp/ui/DashboardActivity.kt
@@ -1,6 +1,9 @@
 package com.mustafayigit.kotlin_milliyetapp.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mustafayigit.kotlin_milliyetapp.R
 import com.mustafayigit.kotlin_milliyetapp.adapter.CategoryNewsAdapter
@@ -25,5 +28,27 @@ class DashboardActivity : AppCompatActivity() {
             )
 
         tabNewsCategory.setupWithViewPager(viewPagerNewsCategory)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_alert -> {
+                Toast.makeText(this, "Hatırlatıcı", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menu_feedback -> {
+                Toast.makeText(this, "Geri Bildirim", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
